d21fd3ea9d8bf9471f0612dfe842e258b83bf5cc
Progr4ming3 <victoremiliano.purificacao@gmail.com>
2026-09-02 21:11:17 -0300

    feat(factories): configurar injecao de dependencias para listagem de clientes
    - Implementadas as factories 'makeListarClientesPfHandler' e 'makeListarClientesPjHandler'.
    - Centralizada a instanciacao sequencial (Repository -> UseCase -> Handler).
    - Mantem a camada de rotas (Next.js API) completamente desacoplada das implementacoes concretas das classes.

--- src/modules/cliente-pf/factories/criar-cliente-pf.factory.ts
+++ src/modules/cliente-pf/factories/criar-cliente-pf.factory.ts
@@ -1,6 +1,12 @@
 import { ClientePfRepository } from "../repositories/cliente-pf.repository";
-import { CriarClientePfUseCase } from "../use-cases/criar-cliente-pf.use-case";
-import { CriarClientePfHandler } from "../handlers/criar-cliente-pf.handler";
+import { CriarClientePfUseCase,ListarClientesPfUseCase } from "../use-cases/criar-cliente-pf.use-case";
+import { CriarClientePfHandler, ListarClientesPfHandler } from "../handlers/criar-cliente-pf.handler";
+
+export const makeListarClientesPfHandler = (): ListarClientesPfHandler => {
+  const repository = new ClientePfRepository();
+  const useCase = new ListarClientesPfUseCase(repository);
+  return new ListarClientesPfHandler(useCase);
+};
 
 export const makeCriarClientePfHandler = (): CriarClientePfHandler => {
   const repository = new ClientePfRepository();
--- src/modules/cliente-pj/factories/criar-cliente-pj.factory.ts
+++ src/modules/cliente-pj/factories/criar-cliente-pj.factory.ts
@@ -1,6 +1,12 @@
 import { ClientePjRepository } from "../repositories/cliente-pj.repository";
-import { CriarClientePjUseCase } from "../use-cases/criar-cliente-pj.use-case";
-import { CriarClientePjHandler } from "../handlers/criar-cliente-pj.handler";
+import { CriarClientePjUseCase, ListarClientesPjUseCase } from "../use-cases/criar-cliente-pj.use-case";
+import { CriarClientePjHandler, ListarClientesPjHandler } from "../handlers/criar-cliente-pj.handler";
+
+export const makeListarClientesPjHandler = (): ListarClientesPjHandler => {
+  const repository = new ClientePjRepository();
+  const useCase = new ListarClientesPjUseCase(repository);
+  return new ListarClientesPjHandler(useCase);
+};
 
 export const makeCriarClientePjHandler = (): CriarClientePjHandler => {
   const repository = new ClientePjRepository();
